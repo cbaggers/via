05c1eb4f3e6ff2e5a5500627b47f7c4ce7c6127f
Baggers <chris.bagley@gmail.com>
2015-06-29 11:24:14 +0200

    works but does not use the :strict option yet

--- base.lisp
+++ base.lisp
@@ -1,43 +1,72 @@
 (in-package #:via)
 
-;; rather than interfaces make proxies
+;; helpers
+(defun mkstr (&rest args)
+  (with-output-to-string (s)
+    (dolist (a args) (princ a s))))
+
+(defun symb (&rest args)
+  (values (intern (apply #'mkstr args))))
 
 ;; a proxy is defined as a list of functions the object must adhere to
-;; a proxy is then a struct that has a field for the object and then one
-;; for each function
-
-(defgeneric defproxy-impl (proxy-name target-type-name))
+;; a proxy is then a struct that has a slot for the object and then one
+;; slot for each function
 
 (defstruct proxy
   (target (error "All proxies must be initialized with a target") :type t))
 
-(defmacro defproxy (name &body func-descriptions)
-  `(progn
-     (defstruct (,(symb name 'proxy) (:include proxy) (:conc-name %prxy-))
-       ,@(loop :for fd :in func-descriptions :collect
-            `(,(first fd) (error ,(format nil "Method ~a for Proxy ~a must be provided"
-                                          name (first fd)))
-               :type function)))
-
-     ,@(loop :for fd :in func-descriptions :collect
-          (let ((slot-name (symb '%prxy- (first fd))))
-            `(defun ,(first fd) (prxy ,@(rest fd))
-               (let ((target (proxy-target prxy)))
-                 (funcall (,slot-name target) target ,@(rest fd))))))))
+(defmacro defproxy (name (&key (strict t)) &body func-descriptions)
+  (declare (ignore strict))
+  (let ((func-descriptions
+         (mapcar (lambda (fd)
+                   (cons (first fd)
+                         (mapcar (lambda (s)
+                                   (if (listp s) s (list s t)))
+                                 (rest fd))))
+                 func-descriptions)))
+    `(progn
+       (defstruct (,(symb name '-proxy) (:include proxy) (:conc-name %prxy-))
+         ,@(loop :for (func-name . args) :in func-descriptions
+              :collect
+              `(,func-name (error ,(format nil "Method ~a for Proxy ~a must be provided"
+                                            func-name name))
+                           :type function)))
+       ,@(loop :for (func-name . args) :in func-descriptions
+            :for arg-names = (mapcar #'first args)
+            :for arg-types = (mapcar #'second args)
+            :collect
+            (let ((slot-name (symb '%prxy- func-name)))
+              `(defun ,func-name (prxy ,@arg-names)
+                 (let* ((target (proxy-target prxy))
+                        (func (,slot-name prxy)))
+                   (funcall func target ,@arg-names))))))))
 
 ;; def-proxy-impl is a macro that lets you define a implementation for a proxy
 ;; for a given type
 ;; you can then use (make-*-proxy x) where * is the type (proxy *) which is
 ;; generic and specialized on the type.
-
-(def-proxy-impl (printable node)
-    :print #'print-node)
-
 (defmacro def-proxy-impl ((proxy-type target-type) &body funcs)
   `(progn
      (defun ,(symb 'make- target-type '-proxy) (x)
-       (,(symb 'make- proxy-type)
+       (,(symb 'make- proxy-type '-proxy)
+         :target x
          ,@funcs))
-     (defmethod proxy ((x ,target-type) (p (eql ',proxy-type)))
+     (defmethod proxify ((x ,target-type) (p (eql ',proxy-type)))
        (declare (ignore p))
        (,(symb 'make- target-type '-proxy) x))))
+
+;; ;; define the proxy
+;; (defproxy printable ()
+;;   (print-it))
+;;
+;; ;; define some example type
+;; (defclass thingy () ())
+;;
+;; ;; define an implementation of the proxy for the type
+;; (def-proxy-impl (printable thingy) :print-it #'print)
+;;
+;; ;; make an instance of the proxy
+;; (defvar test (proxify th 'printable))
+;;
+;; ;; print the printable
+;; (print-it test)
